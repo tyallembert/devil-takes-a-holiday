49c08dc9056250c2b66f9bac5d5d9bb1c7b4bfe3
Ty Allembert <tyallembert@gmail.com>
2023-07-31 14:23:47 -0400

    changed time to pm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <DevilDrawing />
             <BottomText />
             <div className="timeContainer">
-                <p className="time">Monday - Thursday, 5pm - 12pm</p>
+                <p className="time">Monday - Thursday, 5pm - 12am</p>
                 <p className="time">Friday Saturday, 4pm - 1am</p>
                 <p className="time">Sunday, 5pm - 10pm</p>
             </div>
